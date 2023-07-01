0044e104d7b78496c82b632a14faa16739eda288
mungaben <kamaumungai3742@gmail.com>
2023-07-01 22:59:32 +0300

    util table

--- components/ui/DataTable.tsx
+++ components/ui/DataTable.tsx
@@ -121,8 +121,18 @@ const data: Payment[] = [
     status: "failed",
     email: "carmella@hotmail.com",
   },
-
-
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
 ];
 
 export type Payment = {
