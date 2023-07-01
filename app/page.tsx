534624c53d5e84fcb68646aa79fb57933410354b
mungaben <kamaumungai3742@gmail.com>
2023-07-01 22:57:40 +0300

    table.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import REgisterUser from "@/components/REgisterUser";
 import Tbaleava from "@/components/Tbaleava";
+import { Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption, } from "@/components/ui/table";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +20,48 @@ export default function Home() {
         </div>
         <div>
           <REgisterUser />
+        </div>
+        <div className="flex bg-white ">
+          <Table className="flex flex-col ">
+            <TableHead className="p-4 bg-red-400 ">
+              <TableRow className="p-5 bg-gray-600">
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+                <TableCell>
+                  <Image src="/images.png" width={50} height={50} alt={""} />
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </div>
       </div>
     </main>
